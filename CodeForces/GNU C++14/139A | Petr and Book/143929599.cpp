00bfd14a90da081e2685934b25190b611c6e5205
Ankit Gupta <spydiii.hackiez@gmail.com>
2022-10-30 13:26:00 +0530

    Committing files

---
+++ CodeForces/GNU C++14/139A | Petr and Book/143929599.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+ int n;
+ cin>>n;
+ int a[7],sum=0,i=0;
+ for(int j=0;j<7;j++)
+ cin>>a[j];
+ while(sum<n){
+  if(i==7) i=0;
+  sum+=a[i++];
+  }
+  cout<<i;
+  
+ return 0;
+}
