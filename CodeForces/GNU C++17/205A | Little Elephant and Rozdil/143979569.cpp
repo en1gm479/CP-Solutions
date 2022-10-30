9cdc7f84109f9012bee9215ad243fbe4f2d00851
Ankit Gupta <spydiii.hackiez@gmail.com>
2022-10-30 13:25:39 +0530

    Committing files

---
+++ CodeForces/GNU C++17/205A | Little Elephant and Rozdil/143979569.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define rof(a,b,ct) for(int i=a;i<b;i+=ct)
+ 
+ 
+int main(){
+ ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin>>n;
+    int a[n],b[n];
+    rof(0,n,1){
+     cin>>a[i];
+     b[i]=a[i];
+    }
+    sort(b,b+n);
+    if(b[0]==b[1])
+     cout<<"Still Rozdil";
+    else{
+     rof(0,n,1){
+      if(a[i]==b[0]){
+       cout<<i+1;
+       break;
+      }
+     }
+    }
+ return 0;
+}
