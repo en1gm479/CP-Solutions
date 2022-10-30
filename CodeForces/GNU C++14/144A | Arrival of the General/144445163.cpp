7ab9f86755b492850c1463dfde016ef2221527e7
Ankit Gupta <spydiii.hackiez@gmail.com>
2022-10-30 13:24:40 +0530

    Committing files

---
+++ CodeForces/GNU C++14/144A | Arrival of the General/144445163.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define rof(a,b,count) for(int i=a;i<b;i+=count)
+#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define endl "\n"
+ 
+int main(){
+ IOS;
+ 
+ int n,mx=0,mn=INT_MAX,mx_pox=-1,mn_pos=-1;
+ cin>>n;
+ rof(0,n,1){
+  int a;
+  cin>>a;
+  if(a>mx){
+   mx=a;
+   mx_pox=i+1;
+  }
+  if(a<=mn){
+   mn=a;
+   mn_pos=i+1;
+  }
+ }
+ // cout<<mx_pox<<" "<<mn_pos;
+ int t=mx_pox-1+n-mn_pos;
+ mx_pox>mn_pos?cout<<t-1:cout<<t;
+ return 0;
+}
