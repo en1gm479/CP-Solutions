4879b2fe4df9b26a7a307d8d8265c123621259d2
Ankit Gupta <spydiii.hackiez@gmail.com>
2022-10-30 13:27:53 +0530

    Committing files

---
+++ CodeForces/GNU C++14/282A | Bit++/141653430.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+ 
+int main(){
+          int n,a=0;
+          cin>>n;
+          while(n--){
+                    char s[5];
+                    cin>>s;
+                    if(s[0]=='X' && s[1]=='+')
+                    a++;
+                    else if(s[0]=='X' && s[1]=='-')
+                    a--;
+                    else if(s[0]=='+')
+                    ++a;
+                    else if(s[0]=='-')
+                    --a;
+                    
+          }
+          cout<<a;
+          return 0;
+}
