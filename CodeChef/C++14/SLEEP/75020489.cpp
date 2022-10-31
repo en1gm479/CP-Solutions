a22b1db789600ffb9e0ceb87abdc3801f8962d4c
Ankit Gupta <spydiii.hackiez@gmail.com>
2022-10-31 14:15:26 +0530

    Committing files

---
+++ CodeChef/C++14/SLEEP/75020489.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    x<7?cout<<"YES":cout<<"NO";
+	    cout<<endl;
+	}
+	return 0;
+}
