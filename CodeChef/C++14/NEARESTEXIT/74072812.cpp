c0b2deaa801fed3a20dd13a1dd108702091f2348
Ankit Gupta <spydiii.hackiez@gmail.com>
2022-10-31 14:15:37 +0530

    Committing files

---
+++ CodeChef/C++14/NEARESTEXIT/74072812.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(n>50) cout<<"RIGHT";
+	    else cout<<"LEFT";
+	    cout<<endl;
+	}
+	    
+	return 0;
+}
