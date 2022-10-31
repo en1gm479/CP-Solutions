42b6cae8baea8e7ddaccf875029861d0e72ecad4
Ankit Gupta <spydiii.hackiez@gmail.com>
2022-10-31 14:24:23 +0530

    Committing files

---
+++ CodeChef/C++14/REVSTRPT/66204148.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++){
+	    for(int j=n-1;j>i;j--) cout<<" ";
+	    for(int k=0;k<=i;k++) cout<<"*";
+	    cout<<endl;
+	}
+	return 0;
+}
