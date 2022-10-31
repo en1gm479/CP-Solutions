5e354e7fb80d7e5a48a333c5c474f057d4b0860e
Ankit Gupta <spydiii.hackiez@gmail.com>
2022-10-31 14:25:57 +0530

    Committing files

---
+++ CodeChef/C++14/CATSDOGS/57204839.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define rof(a,b,count) for(int i=a;i<b;i+=count)
+#define endl "\n"
+
+int main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	
+	int t;
+	cin>>t;
+	while(t--){
+		long long int c,d,l;
+		cin>>c>>d>>l;
+		bool flag = false;
+		if(l%4==0){
+			int cats_blw = (l-4*d)/4;
+			int cats_abv = c - cats_blw;
+			if(cats_abv<=c && cats_blw<=c && (cats_abv)<=2*d)
+				flag=true;
+			else flag=false;
+		}
+		if (flag) cout<<"yes"<<endl;
+		else cout<<"no"<<endl;
+	}
+	return 0;
+}
