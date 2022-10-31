c3b7c2f8459df6979e15705cb3187b8f48b1c495
Ankit Gupta <spydiii.hackiez@gmail.com>
2022-10-31 14:15:53 +0530

    Committing files

---
+++ CodeChef/C++14/CHEFGAMES/74067544.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c,d;
+	    cin>>a>>b>>c>>d;
+	    if(a==0&&b==0&&c==0&&d==0) cout<<"IN";
+	    else cout<<"OUT";
+	    cout<<endl;
+	}
+	return 0;
+}
