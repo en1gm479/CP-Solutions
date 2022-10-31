a30cf985613dff46f258dad430d3b1d4e42c6233
Ankit Gupta <spydiii.hackiez@gmail.com>
2022-10-31 14:25:47 +0530

    Committing files

---
+++ CodeChef/C++14/GRIDXOR/58281903.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define rof(a,b,count) for(ll i=a;i<b;i+=count)
+#define trav(a, x)  for(auto& a : x)
+#define display(x) trav(a,x) cout<<a<<" ";cout<<endl;
+#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define endl "\n"
+#define ll long long int
+const unsigned int mod = 1000000007;
+
+
+int main(){
+   IOS;
+   int t;
+   cin>>t;
+   while(t--){
+     int n;
+     cin>>n;
+     for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+         cout<<1<<" ";
+      }
+      cout<<endl;
+     }
+   }
+
+}
+
