d2e24a985315d9e748844a7ad0565e630b9b27ca
Ankit Gupta <spydiii.hackiez@gmail.com>
2022-10-31 14:25:37 +0530

    Committing files

---
+++ CodeChef/C++14/LASTWORD/58282812.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define rof(a,b,count) for(ll i=a;i<b;i+=count)
+#define trav(a, x)  for(auto& a : x)
+#define display(x) trav(a,x) cout<<a<<" ";cout<<endl;
+#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define endl "\n"
+#define ll long long int
+const unsigned int mod = 1000000007;
+
+
+int main(){
+   IOS;
+   string s1,s2;
+   while(cin>>s1)
+      s2=s1;
+   cout<<s1.size();
+}
+
