a1708ed96bea542d36024eeb68dad8331419d525
Ankit Gupta <spydiii.hackiez@gmail.com>
2022-10-31 14:23:59 +0530

    Committing files

---
+++ CodeChef/C++14/ANGTRICH/66205532.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b,c;
+	cin>>a>>b>>c;
+	if(a+b+c==180&&a>0&&b>0&&c>0) cout<<"YES";
+	else cout<<"NO";
+	return 0;
+}
