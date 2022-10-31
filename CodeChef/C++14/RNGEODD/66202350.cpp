08e04ff5ac84258590c127c5a55bc7894ec395ef
Ankit Gupta <spydiii.hackiez@gmail.com>
2022-10-31 14:24:38 +0530

    Committing files

---
+++ CodeChef/C++14/RNGEODD/66202350.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int l,r;
+	cin>>l>>r;
+	for(int i=l;i<=r;i++){
+	    if(i&1) cout<<i<<" ";
+	}
+	return 0;
+}
