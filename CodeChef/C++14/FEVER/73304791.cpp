fd8cac698e37baaf34ee48654ab19d99e912a0de
Ankit Gupta <spydiii.hackiez@gmail.com>
2022-10-31 14:16:11 +0530

    Committing files

---
+++ CodeChef/C++14/FEVER/73304791.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    n>98?cout<<"YES":cout<<"NO";
+	    cout<<endl;
+	    
+	}
+	return 0;
+}
