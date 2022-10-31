b0e747d5f3ad955edbd98fac09b0bc73dccf6b04
Ankit Gupta <spydiii.hackiez@gmail.com>
2022-10-31 14:15:14 +0530

    Committing files

---
+++ CodeChef/C++14/STICKBREAK/75041100.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int l,k;
+	    cin>>l>>k;
+	    if(l%k==0) cout<<0;
+	    else cout<<1;
+	    cout<<endl;
+	}
+	return 0;
+}
