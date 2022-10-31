5016647bec665b306414af1409a02f9ae4878c0f
Ankit Gupta <spydiii.hackiez@gmail.com>
2022-10-31 14:22:04 +0530

    Committing files

---
+++ CodeChef/C++14/BADMINTON/57517793.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define rof(a,b,count) for(LL i=a;i<b;i+=count)
+#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define endl "\n"
+typedef long long int LL;
+
+
+int main(){
+	IOS;
+	int n,ctb=0,ctac=0;
+	cin>>n;
+	while(n--){
+		int s;
+		cin>>s;
+		cout<<(s/2)+1<<endl;
+	}
+	return 0;
+}
